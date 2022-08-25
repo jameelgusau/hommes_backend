ffae7be38993e83cea1826bb625be69173c0e58d
jameelgusau <jameelgusau@gmail.com>
2022-08-25 01:20:37 +0100

    Change refresh token time

--- controllers/accounts.controllers.js
+++ controllers/accounts.controllers.js
@@ -273,6 +273,8 @@ function setTokenCookie(res, token) {
   // create cookie with refresh token that expires in 7 days
   const cookieOptions = {
       httpOnly: true,
+      secure:true,
+      sameSite:'none',
       expires: new Date(Date.now() + 7*24*60*60*1000)
   };
   res.cookie('refreshToken', token, cookieOptions);
